5036367707c2e81c9cd3c78c2fa844865c07c62f
zaindgr8 <66069515+zaindgr8@users.noreply.github.com>
2024-06-24 23:51:55 +0400

    1151

---
+++ contactform/contactthanks.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+<html lang="en-US">
+
+<head>
+	<title>Capella</title>
+	
+	<link rel="stylesheet" type="text/css" href="../css/style.css" />
+</head>
+
+<body class="contactbody">
+
+	<div id="page-wrap">
+
+		
+			
+		<br /><br />
+		
+			
+		<h1>Your message has been sent!</h1><br />
+		
+		<p><a href="../contact-us.html">Back to Contact Form</a></p>
+	
+	</div>
+	
+
+</body>
+
+</html>
